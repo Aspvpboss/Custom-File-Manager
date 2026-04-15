dc3db85df2a6e689845673ce679615a60e1f213a
Aspvpboss <b.f.vaughan2026@gmail.com>
2026-04-15 08:39:04 -0400

    created init and cleanup functions, creating renderer for easier text renderering

---
+++ src/main.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+
+class Program{
+
+    static void Main(){
+
+        Init();
+
+
+        while(true){
+
+            if(Console.KeyAvailable && Console.ReadKey(true).KeyChar == 'Q'){
+                break;
+            }
+
+        }
+        
+        Cleanup();
+
+    }
+
+
+    static void Init(){
+
+        Console.Clear();
+        Console.CursorVisible = false;
+
+    }
+    static void Cleanup(){
+
+        Console.Clear();
+        Console.CursorVisible = true;
+
+    }
+}
+
